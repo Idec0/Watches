6f4a1da673eb71b6c75ef509b4858163200ae3e4
Idec0 <brooklynchurchill@exe-coll.ac.uk>
2023-12-20 12:36:48 +0000

    a

--- app/basket/page.jsx
+++ app/basket/page.jsx
@@ -20,12 +20,6 @@ function LoadPage() {
   );
 }
 
-// BasketPage.jsx
-"use client";
-
-import React, { useEffect, useState } from "react";
-import viewWatchURL from "app/watches/page.jsx";
-
 const Bin = (watch) => {
   if (typeof window !== 'undefined' && window.localStorage) {
     let basketList = [];
