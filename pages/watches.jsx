bc613a07b6a9509d38547a42566ea23f136389dc
Idec0 <brooklynchurchill@exe-coll.ac.uk>
2023-12-24 12:37:51 +0000

    loading css

--- pages/watches.jsx
+++ pages/watches.jsx
@@ -365,7 +365,7 @@ const WatchesPage = () => {
     data = [];
     imagePositionMap = {};
     watchListIndex = 0;
-    return <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Loading...</div>;
+    return <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: "5vh" }}>Loading...</div>;
   }
 
   const getImgStyle = (index) => {
