e7600c2792644b55237726d877893f09af981014
Idec0 <brooklynchurchill@exe-coll.ac.uk>
2024-02-21 12:23:16 +0000

    retrying mobile

--- components/base.jsx
+++ components/base.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import {Head} from 'next/document';
 
 const Navbar = ({ appVisible, setAppVisible }) => {
   const [loggedIn, setLoggedIn] = useState(false);
